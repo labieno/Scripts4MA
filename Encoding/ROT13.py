2693b5d6cdb109d76c422f163ac0710de850c66a
labieno <haeminrto@hotmail.com>
2023-03-09 11:34:11 +0100

    Encoding folder

---
+++ Encoding/ROT13.py
@@ -0,0 +1,13 @@
+#ROT13 with custom alphabet:
+def decryptit(string):
+    key = ""
+    decrypted_str = ""
+    for c in string:
+        i = key.find(c)
+        if (i + 13) < len(key):
+            v8 = i + 13
+        else:
+            v8 = i - len(key) + 13
+        decrypted_str += key[v8]
+    
+    print(decrypted_str)
